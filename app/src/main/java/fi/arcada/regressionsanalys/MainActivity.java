2dafdf333d616ee157f1daee334ad1f0b0fe5bf1
Rasmus Lagerqvist <rasmus.o.lagerqvist@gmail.com>
2022-04-19 13:06:16 +0200

    Del 2. Inlämning.

--- app/src/main/java/fi/arcada/regressionsanalys/MainActivity.java
+++ app/src/main/java/fi/arcada/regressionsanalys/MainActivity.java
@@ -7,16 +7,21 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 public class MainActivity extends AppCompatActivity {
 
     // Två datamängder med längd och skonummer för 26 olika personer
     // hänger ihop på så vis att xData[0] och yData[0] är skonummer och längd  för den första personen, osv.
     // Observera att de är primitiva arrays, inte ArrayLists, så de behandlas lite annorlunda.
-    double[] xData = { 47,  42,  43,  42,  41,  48,  46,  44,  42,  43,  39,  43,  39,  42,  44,  45,  43,  44,  45,  42,  43,  32,  48,  43,  45,  45};
-    double[] yData = { 194, 188, 181, 177, 182, 197, 179, 176, 177, 188, 164, 171, 170, 180, 171, 185, 179, 182, 180, 178, 178, 148, 197, 183, 179, 198};
+
+    // Reminder! Switched names xData -> yData
+    double[] xData = {47,  42,  43,  42,  41,  48,  46,  44,  42,  43,  39,  43,  39,  42,  44,  45,  43,  44,  45,  42,  43,  32,  48,  43,  45,  45};
+    double[] yData = {194, 188, 181, 177, 182, 197, 179, 176, 177, 188, 164, 171, 170, 180, 171, 185, 179, 182, 180, 178, 178, 148, 197, 183, 179, 198};
 
     // Deklarera yValue för längd, Denna variabel ska sedan få ett värde som hämtas från en EditText-box i appens GUI
-    double yValue;
+    double yValue, testX;
 
     // Deklarera övriga variabler och objekt du behöver, t.ex. TextViews osv.
     TextView statementText;
@@ -68,7 +73,8 @@ public class MainActivity extends AppCompatActivity {
             // Instansiera regressionLine t.ex. så här:
 
             RegressionLine regLine = new RegressionLine(xData, yData);
-            statementText.setText("Your shoe size is probably "+ regLine.getX(yValue));
+            statementText.setText("Your shoe size is probably "+ String.format("%.2f", regLine.getX(yValue))+ "!");
+            // statementText.setText(String.valueOf(Arrays.stream(xData).sum()));
 
             // Ta emot användarens input (längd) och spara i yValue
             // Använd ett try/catch-block för NumberFormatException så att appen inte crashar
@@ -83,6 +89,28 @@ public class MainActivity extends AppCompatActivity {
         } catch (NumberFormatException e) {
             e.printStackTrace();
         }
+    }
+
+    public double calcSum() {
+        try {
+            double sum = 0;
+            for (int i = 0; i < xData.length; i++) {
+                sum = sum + xData[i] * yData[i];
+            }
+            testX = sum;
+
+
+            // sumAll Calc
+            /* double sum = 0;
+            for (int i = 0; i < xData.length; i++) {
+                sum = sum + (xData[i] * yData[i]);
+            }
+            testX = sum; */
+
+        } catch (NumberFormatException e) {
+            e.printStackTrace();
+        }
+        return testX;
     }
 
 }
--- app/src/main/java/fi/arcada/regressionsanalys/RegressionLine.java
+++ app/src/main/java/fi/arcada/regressionsanalys/RegressionLine.java
@@ -1,19 +1,58 @@
 package fi.arcada.regressionsanalys;
 
+import java.util.Arrays;
+import java.util.stream.LongStream;
+
 public class RegressionLine {
 
     // deklarera k, m, x  och correlationCoefficient som double
 
-    double x, y, k, m, correlationCoefficient;
+    double k, m, x, correlationCoefficient;
     double[] xVals, yVals;
 
     public RegressionLine(double[] xVals, double[] yVals) {
+        double sumAll, sumX, sumY, sumSquared, allSquared, arrLength;
 
+        // Amount of pairs
+        arrLength = xVals.length;
+
+        // Sum of all values x*y
+        double sum1 = 0;
+        for (int i = 0; i < xVals.length; i++) {
+            sum1 = sum1 + xVals[i] * yVals[i];
+        }
+        sumAll = sum1;
+
+        // Sum of all x values
+        sumX = Arrays.stream(xVals).sum();
+
+        // Sum of all y values
+        sumY = Arrays.stream(yVals).sum();
+
+        // Sum of all x squared
+        double sum = 0;
+        for (double xVal : xVals) {
+            sum = sum + xVal * xVal;
+        }
+        allSquared = sum;
+
+        // Sum of x squared
+        sumSquared = sumX * sumX;
+
+        k = (arrLength * sumAll - sumX * sumY)/(arrLength * allSquared - sumSquared);
+        m = (sumY/arrLength) - (k * sumX/arrLength);
     }
 
     public double getX(double yValue) {
+        x = (yValue - m)/k;
         return x;
     }
+
+    // Del 3 - WIP
+    public double getCorrelationCoefficient(double yValue) {
+        return 0.0;
+    }
+
 
     // Skapa en konstruktor som tar emot data-arrays för x och y
     // Uträkningen för k och m kan ske i konstruktorn m.h.a.
